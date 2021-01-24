269fbb933f4d736928d59078763ce6a0df1a8d05
Mehdi Agounine <mehdi.agounine@gmail.com>
2021-01-24 23:17:49 +0100

    Ajout de la liste des produits du vendeurs et des fixtures produit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,22 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
+use App\Entity\Produit;
 use App\Entity\Vendeur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-         
-       
-        $tabVendeur = ['Vendeur1', 'Vendeur2', 'Vendeur3', 'Vendeur4', 'Vendeur5', 'Vendeur6', 'Vendeur7', 'Vendeur8', 'Vendeur9', 'Vendeur10', 'Vendeur11', 'Vendeur12'];
-        
+       for($i = 1 ; $i <=30 ; $i++){
+           $tabVendeur[] = 'Vendeur'.$i;
+       }        
 
-        foreach ($tabVendeur as $key => $value) {
+        foreach ($tabVendeur as $value) {
 
+            
             $vendeur = new Vendeur();
+            $password = $this->encoder->encodePassword($vendeur, $value);
             $vendeur->setNom($value)
             ->setPrenom($value)
             ->setRaisonSociale($value)
@@ -26,12 +37,48 @@ class AppFixtures extends Fixture
             ->setAdresse(['Adresse de'.$value])
             ->setDescription('Description du '.$value)
             ->setEmail($value.'@gmail.com')
-            ->setPassword($value) 
+            ->setPassword($password)
             ;
-            
             $manager->persist($vendeur);
 
+            for($i = 1 ; $i <=10 ; $i++){
+            $produit = new Produit();
+            $produit->setNom('produit '.$i.' du '.$value)
+            ->setDescription('Une Description Ici')
+            ->setStock(rand(50,500))
+            ->setPoidUnitaire(300)
+            ->setPrixUnitaireHT(rand(500,2000)/100)
+            ->setVendeur($vendeur);
+            
+            $manager->persist($produit);
+
+            }
         }
+
+        for($i = 1 ; $i <=30 ; $i++){
+            $tabClient[] = 'Client'.$i;
+        }        
+ 
+         foreach ($tabClient as $value) {
+ 
+             
+             $client = new Client();
+             $password = $this->encoder->encodePassword($client, $value);
+             $client->setNom($value)
+             ->setPrenom($value)
+             ->setTelephone(mt_rand(1111111111,9999999999))
+             ->setAdresse([
+                 'Adresse de'.$value
+                 ])
+             ->setPaiement(['mastercard'
+             ])
+             ->setEmail($value.'@gmail.com')
+             ->setPassword($password)
+             ;
+             
+             $manager->persist($client);
+ 
+         }
 
         $manager->flush();
     }
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -10,6 +10,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Produit
 {
+
+    public function __construct()
+    {
+        $this->setTVA(0.055);
+    }
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -28,7 +33,7 @@ class Produit
     private $stock;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255,nullable=true)
      */
     private $image;
 
@@ -38,17 +43,17 @@ class Produit
     private $poidUnitaire;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="float")
      */
-    private $prixUnitaireHt;
+    private $prixUnitaireHT;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255,nullable=true)
      */
     private $description;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="float")
      */
     private $TVA;
 
@@ -115,14 +120,14 @@ class Produit
         return $this;
     }
 
-    public function getPrixUnitaireHt(): ?int
+    public function getPrixUnitaireHT(): ?int
     {
-        return $this->prixUnitaireHt;
+        return $this->prixUnitaireHT;
     }
 
-    public function setPrixUnitaireHt(int $prixUnitaireHt): self
+    public function setPrixUnitaireHT(int $prixUnitaireHT): self
     {
-        $this->prixUnitaireHt = $prixUnitaireHt;
+        $this->prixUnitaireHT = $prixUnitaireHT;
 
         return $this;
     }
