1524ac0178db650dda2b4abe7948dcb79d853359
Hophelie <hophelie.bourgeois@hotmail.fr>
2021-01-28 09:00:23 +0100

    Ajout Easy admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Client;
+use App\Entity\Produit;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -13,56 +15,30 @@ use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
-    /**
-     * @Route("/admin")
+   /**
+     * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-        // redirect to some CRUD controller
-        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        // $routeBuilder = $this->get(AdminUrlGenerator::class);
 
-        return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
+        // return $this->redirect($routeBuilder->setController(ProduitCrudController::class)->generateUrl());
 
-        // you can also redirect to different pages depending on the current user
-        if ('jane' === $this->getUser()->getUsername()) {
-            return $this->redirect('...');
-        }
-
-        // you can also render some template to display a proper Dashboard
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        return $this->render('some/path/my-dashboard.html.twig');
+         return parent::index();
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            // the name visible to end users
-            ->setTitle('Tableau de bord')
-           
-            // the path defined in this method is passed to the Twig asset() function
-            ->setFaviconPath('favicon.svg')
+            ->setTitle('ProjetFilRouge');
+    }
 
-            // the domain used by default is 'messages'
-            ->setTranslationDomain('my-custom-domain')
-
-            // there's no need to define the "text direction" explicitly because
-            // its default value is inferred dynamically from the user locale
-            ->setTextDirection('ltr')
-
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
-            ->renderContentMaximized()
-
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
-            ->renderSidebarMinimized()
-
-            // by default, all backend URLs include a signature hash. If a user changes any
-            // query parameter (to "hack" the backend) the signature won't match and EasyAdmin
-            // triggers an error. If this causes any issue in your backend, call this method
-            // to disable this feature and remove all URL signature checks
-            ->disableUrlSignatures()
-        ;
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::linktoDashboard('Dashboard','fa fa-home');
+        yield MenuItem::linkToCrud('Client','fa fa-user', Client::class);
+        yield MenuItem::linkToCrud('Produits','fa fa-list', Produit::class);     
     }
 
 }
